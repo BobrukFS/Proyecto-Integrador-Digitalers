53cbbbabf5a249f9ae352b19e6789935ecfb8819
Exequiel Bobruk <bobrukfs@gmail.com>
2023-10-14 13:36:06 -0300

    fix: Configure el vite

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,5 +1,21 @@
+import { resolve } from 'node:path'
+
 module.exports = {
     server: {
       port: 5500,
     },
+    build: {
+      emptyOutDir: true,
+      rollupOptions: {
+          input: {
+              carrito: resolve('./src/pages/checkout.html'),
+              contacto: resolve('./src/pages/contact.html'),
+              // favoritos: resolve('./pages/favoritos.hml'),
+              nosotros: resolve('./src/pages/about.html'),
+              productos: resolve('./src/pages/store.html'),
+
+              index: resolve('index.html')
+          }
+      }
+  }
   };
